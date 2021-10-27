925957399cc97cd445f600d88ef1ed916ee4076d
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2021-10-26 23:28:46 -0500

    Initial install laravel-vuejs3

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!-- NEW -->
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
+        <script src="{{ asset('/js/app.js') }}" defer></script>
+        <!--  -->
+        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        @routes
+    </head>
+    <body class="font-sans antialiased">
+        @inertia
+        <h2>Walter White</h2>
+    </body>
+</html>
